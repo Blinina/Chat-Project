3f2136519ed4a4c11f1348ff6261c534f570c73c
Blinina <anastasia.paancake@gmail.com>
2022-09-27 08:46:00 +0300

    27.09 commit

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Button } from 'react-bootstrap';
+import { Navbar, Button, Container } from 'react-bootstrap';
 import useAuth from "../hooks/authHooks";
 import { useTranslation } from 'react-i18next';
 
@@ -8,10 +8,10 @@ export default function Navigation() {
     const auth = useAuth();
     return (
         <Navbar className="shadow-sm navbar navbar-light bg-white">
-            <div className="container">
+            <Container className="container">
                 <a className="navbar-brand" href="login">Hexlet Chat</a>
-                {auth.loggedIn ? <Button onClick={() => auth.logOut()} className="btn btn-primary">{t('logOut')}</Button> : null}
-            </div>
+                {auth.loggedIn ? <Button onClick={() => auth.logOut()} className="btn-primary">{t('logOut')}</Button> : null}
+            </Container>
         </Navbar >
     )
 }
