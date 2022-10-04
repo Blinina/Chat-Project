6da59d7180e4c0df48e563dd4df3576c2bb045ed
Blinina <anastasia.paancake@gmail.com>
2022-10-04 19:25:09 +0300

    modal update2

--- frontend/src/components/Chat/modals/Add.jsx
+++ frontend/src/components/Chat/modals/Add.jsx
@@ -76,7 +76,7 @@ export default function Add() {
             name="body"
             className={formValid ? 'mb-2' : 'form-control is-invalid mb-2'}
           />
-          {/* <Form.Label className="visually-hidden" htmlFor="body">{t('modal.name')}</Form.Label> */}
+          <Form.Label className="visually-hidden" htmlFor="body">{t('modal.name')}</Form.Label>
           <div className="invalid-fb">{t(validationError)}</div>
         </Form.Group>
         <div className="d-flex justify-content-end">
